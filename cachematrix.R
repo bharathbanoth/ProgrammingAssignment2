97ca8b10350c9a2bf34768e557a886448e8449da
Bharath Kumar Banoth <bharathbanoth@users.noreply.github.com>
2015-11-19 13:08:25 +0530

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,14 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions calculate the inverse of a matrix and saves it
+## to the cache such that the next time the user attempts to calculate the
+## matrix inverse, the previously saved value is returned instead of
+## repeating the calculation.
 
-## Write a short comment describing this function
-
+## This function creates a special "matrix" object, which is really a list 
+## containing a function to
+## 1. set the value of the matrix
+## 2. get the value of the matrix
+## 3. set the value of the inverse
+## 4. get the value of the inverse
 
 makeCacheMatrix <- function(x = matrix()) {
         ## create a matrix object x and some associated sub-functions/methods
